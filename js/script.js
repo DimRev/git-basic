58f71620059861faf70b4a754726a611bef05229
Dima Rev <dimarev444@gmail.com>
2023-11-15 16:03:53 +0200

    added limit of 400px to ball size

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 function onBallClick() {
   const elBall = document.querySelector('.ball')
   var currSize = parseInt(elBall.offsetWidth, 10)
-  
+  console.log(currSize);
+  if (currSize > 400) {
+    elBall.style.width = 100 + 'px'
+    elBall.style.height = 100 + 'px'
+    return
+  }
+
   elBall.style.width = currSize + 50 + 'px'
   elBall.style.height = currSize + 50 + 'px'
 }
