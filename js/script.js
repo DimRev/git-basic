5bc7fc709a410fda167344053884f5e5827d3ab6
Dima Rev <dimarev444@gmail.com>
2023-11-15 15:47:18 +0200

    added onBallClick() funtion to grow by 50px

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
-function onBallClick(){
-  console.log('test');
+function onBallClick() {
+  const elBall = document.querySelector('.ball')
+  var currSize = parseInt(elBall.offsetWidth, 10)
+  
+  elBall.style.width = currSize + 50 + 'px'
+  elBall.style.height = currSize + 50 + 'px'
 }
