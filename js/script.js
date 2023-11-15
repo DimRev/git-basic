9cc2351aee571d589f3b51a6f90fafdab87777d4
Dima Rev <dimarev444@gmail.com>
2023-11-15 17:02:05 +0200

    add onSwapBalls() func

--- js/script.js
+++ js/script.js
@@ -15,3 +15,24 @@ function onBallClick(elBall, maxDiameter) {
   elBall.style.height = currSize + rdmIncrement + 'px'
   elBall.innerText = currSize + rdmIncrement
 }
+
+function onSwapBalls() {
+  const elBall1 = document.querySelector('.ball-1')
+  const elBall2 = document.querySelector('.ball-2')
+  var tempSize
+  var tempColor
+  var tempInnerText
+  tempSize = parseInt(elBall1.offsetWidth, 10)
+  tempColor = elBall1.style.backgroundColor
+  tempInnerText = elBall1.innerText
+
+  elBall1.style.width = parseInt(elBall2.offsetWidth, 10) + 'px'
+  elBall1.style.height = parseInt(elBall2.offsetHeight, 10) + 'px'
+  elBall1.style.backgroundColor = elBall2.style.backgroundColor
+  elBall1.innerText = elBall2.innerText
+
+  elBall2.style.width = tempSize + 'px'
+  elBall2.style.height = tempSize + 'px'
+  elBall2.style.backgroundColor = tempColor
+  elBall2.innerText = tempInnerText
+}
