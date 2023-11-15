fde7cf312c650e3306ce8c8e1ca8d1690d05458b
Dima Rev <dimarev444@gmail.com>
2023-11-15 16:27:16 +0200

    add rdmIncrement to ball radius increment

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 function onBallClick() {
   const elBall = document.querySelector('.ball')
   var currSize = parseInt(elBall.offsetWidth, 10)
-  console.log(currSize);
+  var rdmIncrement = getRandomIntegerInc(20, 60)
+  console.log(currSize)
   if (currSize > 400) {
     elBall.style.width = 100 + 'px'
     elBall.style.height = 100 + 'px'
     return
   }
 
-  elBall.style.width = currSize + 50 + 'px'
-  elBall.style.height = currSize + 50 + 'px'
+  elBall.style.width = currSize + rdmIncrement + 'px'
+  elBall.style.height = currSize + rdmIncrement + 'px'
 }
