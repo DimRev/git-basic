01c986640285c94fc3a5b01e1ee6f6a23a63ecd5
Dima Rev <dimarev444@gmail.com>
2023-11-15 17:15:42 +0200

    add onReduceBalls function

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ function onSwapBalls() {
   var tempSize
   var tempColor
   var tempInnerText
-  
+
   tempSize = parseInt(elBall1.offsetWidth, 10)
   tempColor = elBall1.style.backgroundColor
   tempInnerText = elBall1.innerText
@@ -37,4 +37,27 @@ function onSwapBalls() {
   elBall2.style.height = tempSize + 'px'
   elBall2.style.backgroundColor = tempColor
   elBall2.innerText = tempInnerText
+}
+
+function onReduceBalls(minDiameter = 100) {
+  const elBall1 = document.querySelector('.ball-1')
+  const elBall2 = document.querySelector('.ball-2')
+
+  var currSize1 = parseInt(elBall1.offsetWidth, 10)
+  var currSize2 = parseInt(elBall2.offsetWidth, 10)
+
+  var rdmIncrement = getRandomIntegerInc(20, 60)
+
+  if (currSize1 - rdmIncrement > 100) {
+    elBall1.style.width = currSize1 - rdmIncrement + 'px'
+    elBall1.style.height = currSize1 - rdmIncrement + 'px'
+    elBall1.style.backgroundColor = getRandomColor()
+    elBall1.innerText = currSize1 - rdmIncrement
+  }
+  if (currSize2 - rdmIncrement > 100) {
+    elBall2.style.width = currSize2 - rdmIncrement + 'px'
+    elBall2.style.height = currSize2 - rdmIncrement + 'px'
+    elBall2.style.backgroundColor = getRandomColor()
+    elBall2.innerText = currSize2 - rdmIncrement
+  }
 }
