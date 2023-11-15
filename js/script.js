ebb2b6f440bf1dad857660e81bc40fad9e3c239a
Dima Rev <dimarev444@gmail.com>
2023-11-15 16:40:17 +0200

    change inner text to ball shrink case

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 function onBallClick(elBall) {
   var currSize = parseInt(elBall.offsetWidth, 10)
   var rdmIncrement = getRandomIntegerInc(20, 60)
-  
+
   elBall.style.backgroundColor = getRandomColor()
-  
-  if (currSize > 400) {
+
+  if (currSize + rdmIncrement > 400) {
     elBall.style.width = 100 + 'px'
     elBall.style.height = 100 + 'px'
+    elBall.innerText = 100
     return
   }
 
